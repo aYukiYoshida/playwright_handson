12362e2625b6b28b5c30a8b9139183ae66919a67
aYukiYoshida <yuki.yoshida@access-company.com>
2023-04-30 22:19:32 +0900

    Update sample testing file

--- tests/teseus.spec.ts
+++ tests/teseus.spec.ts
@@ -2,43 +2,49 @@ import { test, expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
   // arranges
-  await page.goto("http://localhost:8888/");
+  await page.goto("/");
 });
 
-test.describe("Login to", () => {
-  test("ログインページに遷移する", async ({ page }) => {
-    await expect(page).toHaveURL(/login$/);
+test.describe("Access to TESEUS", () => {
+  test("ログインページに遷移する", async ({ page, baseURL }) => {
+    await expect(page).toHaveURL(baseURL + "/login");
   });
 
-  test("正当なユーザー名とパスワードでログインする", async ({ page }) => {
+  test("正当なユーザー名とパスワードでログインする", async ({
+    page,
+    baseURL,
+  }) => {
     // actions
     await page.getByTestId("username").fill("pulsar");
     await page.getByTestId("password").fill("neutronstar");
     await page.getByRole("button", { name: "Login" }).click();
 
-    // assertions
+    // asserts
+    await expect(page).toHaveURL(baseURL + "/status");
     await expect(page.getByTestId("status")).toHaveText("Success to login.");
   });
 
-  test("登録がないユーザー名でログインする", async ({ page }) => {
+  test("登録がないユーザー名でログインする", async ({ page, baseURL }) => {
     // actions
     await page.getByTestId("username").fill("magnetar");
     await page.getByTestId("password").fill("neutronstar");
     await page.getByRole("button", { name: "Login" }).click();
 
-    // assertions
+    // asserts
+    await expect(page).toHaveURL(baseURL + "/status");
     await expect(page.getByTestId("status")).toHaveText(
       "Incorrect username or password."
     );
   });
 
-  test("不正なパスワードでログインする", async ({ page }) => {
+  test("不正なパスワードでログインする", async ({ page, baseURL }) => {
     // actions
-    await page.getByTestId("username").fill("magnetar");
+    await page.getByTestId("username").fill("pulsar");
     await page.getByTestId("password").fill("blackhole");
     await page.getByRole("button", { name: "Login" }).click();
 
-    // assertions
+    // asserts
+    await expect(page).toHaveURL(baseURL + "/status");
     await expect(page.getByTestId("status")).toHaveText(
       "Incorrect username or password."
     );
