be8ea3f0c5d0ad029cc88427b0ace27cc6bb974c
aYukiYoshida <yuki.yoshida@access-company.com>
2023-05-16 17:12:05 +0900

    Introduce test hooks of test.step

--- tests/teseus.spec.ts
+++ tests/teseus.spec.ts
@@ -6,8 +6,11 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe("Access to TESEUS", () => {
-  test("ログインページに遷移する", async ({ page, baseURL }) => {
-    await expect(page).toHaveURL(baseURL + "/login");
+  test("アプリケーションを開く", async ({ page, baseURL }) => {
+    // asserts
+    await test.step("ログインページに遷移する", async () => {
+      await expect(page).toHaveURL(baseURL + "/login");
+    });
   });
 
   test("正当なユーザー名とパスワードでログインする", async ({
@@ -15,38 +18,56 @@ test.describe("Access to TESEUS", () => {
     baseURL,
   }) => {
     // actions
-    await page.getByTestId("username").fill("pulsar");
-    await page.getByTestId("password").fill("neutronstar");
-    await page.getByRole("button", { name: "Login" }).click();
+    await test.step("ユーザー名とパスワードを入力", async () => {
+      await page.getByTestId("username").fill("pulsar");
+      await page.getByTestId("password").fill("neutronstar");
+    });
+    await test.step("ログインボタンを押下する", async () => {
+      await page.getByRole("button", { name: "Login" }).click();
+    });
 
     // asserts
-    await expect(page).toHaveURL(baseURL + "/status");
-    await expect(page.getByTestId("status")).toHaveText("Success to login.");
+    await test.step("ステータスページに遷移し、ログインに成功する", async () => {
+      await expect(page).toHaveURL(baseURL + "/status");
+      await expect(page.getByTestId("status")).toHaveText("Success to login.");
+    });
   });
 
   test("登録がないユーザー名でログインする", async ({ page, baseURL }) => {
     // actions
-    await page.getByTestId("username").fill("magnetar");
-    await page.getByTestId("password").fill("neutronstar");
-    await page.getByRole("button", { name: "Login" }).click();
+    await test.step("ユーザー名とパスワードを入力", async () => {
+      await page.getByTestId("username").fill("magnetar");
+      await page.getByTestId("password").fill("neutronstar");
+    });
+    await test.step("ログインボタンを押下する", async () => {
+      await page.getByRole("button", { name: "Login" }).click();
+    });
 
     // asserts
-    await expect(page).toHaveURL(baseURL + "/status");
-    await expect(page.getByTestId("status")).toHaveText(
-      "Incorrect username or password."
-    );
+    await test.step("ステータスページに遷移し、ログインに失敗する", async () => {
+      await expect(page).toHaveURL(baseURL + "/status");
+      await expect(page.getByTestId("status")).toHaveText(
+        "Incorrect username or password."
+      );
+    });
   });
 
   test("不正なパスワードでログインする", async ({ page, baseURL }) => {
     // actions
-    await page.getByTestId("username").fill("pulsar");
-    await page.getByTestId("password").fill("blackhole");
-    await page.getByRole("button", { name: "Login" }).click();
+    await test.step("ユーザー名とパスワードを入力", async () => {
+      await page.getByTestId("username").fill("pulsar");
+      await page.getByTestId("password").fill("blackhole");
+    });
+    await test.step("ログインボタンを押下する", async () => {
+      await page.getByRole("button", { name: "Login" }).click();
+    });
 
     // asserts
-    await expect(page).toHaveURL(baseURL + "/status");
-    await expect(page.getByTestId("status")).toHaveText(
-      "Incorrect username or password."
-    );
+    await test.step("ステータスページに遷移し、ログインに失敗する", async () => {
+      await expect(page).toHaveURL(baseURL + "/status");
+      await expect(page.getByTestId("status")).toHaveText(
+        "Incorrect username or password."
+      );
+    });
   });
 });
